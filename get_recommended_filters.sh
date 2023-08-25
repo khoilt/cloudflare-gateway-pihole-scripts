bf8279f440df2d018400fbd427dd1c247b274675
khoilt <143152161+khoilt@users.noreply.github.com>
2023-08-25 10:22:22 +0700

    Update get_recommended_filters.sh
    Bổ sung block lists
    [1] https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
    [2] https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -12,6 +12,8 @@ urls=(
   https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt
   https://adaway.org/hosts.txt
   https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+  https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt  #bổ sung
+  https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt            #bổ sung
 )
 
 # loop through the urls and download each file with curl
